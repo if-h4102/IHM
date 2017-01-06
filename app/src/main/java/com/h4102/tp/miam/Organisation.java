7e39a87101c1d4c80a66d9cf0ff3f53a245cb6e5
HugoMoy <hugo.moynac@gmail.com>
2017-01-06 10:35:01 +0100

    test orga

--- app/src/main/java/com/h4102/tp/miam/Organisation.java
+++ app/src/main/java/com/h4102/tp/miam/Organisation.java
@@ -2,6 +2,7 @@ package com.h4102.tp.miam;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.content.Intent;
 
 public class Organisation extends AppCompatActivity {
 
@@ -9,5 +10,21 @@ public class Organisation extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_organisation);
+
+        Button Send = (Button) findViewById(R.id.buttonSend);
+        Button Cancel = (Button) findViewById(R.id.buttonCancel);
+
+        Send.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v) {
+                Intent intent = new Intent(v.getContext(), Restaurant.class);
+                startActivity(intent);
+            }
+        });
+
+        Cancel.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v){
+                Intent intent = new Intent(v.getContext(), MapRestaurant.class);
+                startActivity(intent);
+            }
     }
 }
