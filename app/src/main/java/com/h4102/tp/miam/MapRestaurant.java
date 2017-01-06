a6063a883c4adfe77246df21aea81f6c4931ab11
Nicolas <nicolas.six@insa-lyon.fr>
2017-01-06 11:39:03 +0100

    temp commit

--- app/src/main/java/com/h4102/tp/miam/MapRestaurant.java
+++ app/src/main/java/com/h4102/tp/miam/MapRestaurant.java
@@ -4,8 +4,11 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
 import com.google.android.gms.maps.MapFragment;
+import com.google.android.gms.maps.model.BitmapDescriptorFactory;
 import com.google.android.gms.maps.model.CameraPosition;
 import com.google.android.gms.maps.model.LatLng;
+import com.google.android.gms.maps.model.Marker;
+import com.google.android.gms.maps.model.MarkerOptions;
 
 public class MapRestaurant extends AppCompatActivity {
 
@@ -14,14 +17,16 @@ public class MapRestaurant extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_map_restaurant_bis);
 
-//        CameraPosition cameraPosition = new CameraPosition.Builder()
-//                .target(new LatLng(-33, 150))
-//                .zoom(13)
-//                .build();
-//
-//        MapFragment mapFragment = (MapFragment) getFragmentManager()
-//                .findFragmentById(R.id.map);
-//
+        CameraPosition cameraPosition = new CameraPosition.Builder()
+                .target(new LatLng(-33, 150))
+                .zoom(13)
+                .build();
+
+        MapFragment mapFragment = (MapFragment) getFragmentManager()
+                .findFragmentById(R.id.map);
+
+
+        //Marker newmarker = mapFragment. .addMarker(new MarkerOptions().position(latlng).title("marker title").icon(BitmapDescriptorFactory.fromResource(R.drawable.marker_for_map_purpul)));
 
 
     }
