e77c443f9e0d95915fe627de523cf0886f298c90
Nicolas <nicolas.six@insa-lyon.fr>
2017-01-06 09:18:24 +0100

    add image to main view and link to restaurant activity

--- app/src/main/java/com/h4102/tp/miam/MainActivity.java
+++ app/src/main/java/com/h4102/tp/miam/MainActivity.java
@@ -1,13 +1,22 @@
 package com.h4102.tp.miam;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import com.h4102.tp.miam.Restaurant;
 
 public class MainActivity extends AppCompatActivity {
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+
+    public void goToRestaurantActivity(View view){
+        Intent intent = new Intent(this, Restaurant.class);
+        startActivity(intent);
     }
 }
