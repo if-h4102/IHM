c0b8aa9860975936d0e77378ecd5fc49e6f80367
Charles Samborski <demurgos@demurgos.net>
2017-01-09 22:07:17 +0000

    Add Invitations activity

---
+++ app/src/main/java/com/h4102/tp/miam/Invitations.java
@@ -0,0 +1,15 @@
+package com.h4102.tp.miam;
+
+import android.app.ListActivity;
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.widget.ArrayAdapter;
+
+public class Invitations extends AppCompatActivity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_invitations);
+
+    }
+}
