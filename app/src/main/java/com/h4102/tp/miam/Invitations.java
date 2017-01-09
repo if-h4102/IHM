da50af39d2972675bfaa9b09351c20f097506b59
Charles Samborski <demurgos@demurgos.net>
2017-01-09 23:18:02 +0000

    Use simple array adapter

--- app/src/main/java/com/h4102/tp/miam/Invitations.java
+++ app/src/main/java/com/h4102/tp/miam/Invitations.java
@@ -3,7 +3,12 @@ package com.h4102.tp.miam;
 import android.app.ListActivity;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
+import android.widget.ListView;
+
+import java.util.ArrayList;
+import java.util.Collections;
 
 public class Invitations extends AppCompatActivity {
     @Override
@@ -11,5 +16,13 @@ public class Invitations extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_invitations);
 
+        final ListView listview = (ListView) findViewById(R.id.invitations_listview);
+        String[] values = new String[] {"Grillon", "Olivier", "Prévert"};
+
+        final ArrayList<String> list = new ArrayList<>();
+        Collections.addAll(list, values);
+
+        final ArrayAdapter<String> adapter = new ArrayAdapter<String>(this, android.R.layout.simple_list_item_1, list);
+        listview.setAdapter(adapter);
     }
 }
