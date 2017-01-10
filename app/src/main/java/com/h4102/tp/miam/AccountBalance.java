558177006cf44443ef38b6d37730c08258c10912
SebastienDiGiovanni <sebastien.di-giovanni@insa-lyon.fr>
2017-01-10 16:17:08 +0100

    Balance window fixed

--- app/src/main/java/com/h4102/tp/miam/AccountBalance.java
+++ app/src/main/java/com/h4102/tp/miam/AccountBalance.java
@@ -1,7 +1,7 @@
 package com.h4102.tp.miam;
 
-import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
 
 public class AccountBalance extends AppCompatActivity {
 
