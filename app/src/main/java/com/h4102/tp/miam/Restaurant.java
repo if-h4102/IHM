86873fca9396353110d17ed3deac5d1a81af1d28
frobion <francois.robion@insa-lyon.fr>
2017-01-12 11:14:19 +0100

    Put waiting time instead of price in restaurants

--- app/src/main/java/com/h4102/tp/miam/Restaurant.java
+++ app/src/main/java/com/h4102/tp/miam/Restaurant.java
@@ -1,8 +1,10 @@
 package com.h4102.tp.miam;
 
 import android.app.ListActivity;
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.view.View;
 import android.widget.ArrayAdapter;
 
 public class Restaurant extends AppCompatActivity {
@@ -12,5 +14,30 @@ public class Restaurant extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_restaurant);
 
+    }
+
+    public void goToPrevertDetails (View view){
+        Intent intent = new Intent(this, PrevertMealActivity.class);
+        startActivity(intent);
+    }
+
+    public void goToRestaurantInsaDetails (View view){
+        //Intent intent = new Intent(this, RestaurantInsaMealActivity.class);
+        //startActivity(intent);
+    }
+
+    public void goToGrillonDetails (View view){
+        //Intent intent = new Intent(this, GrillonMealActivity.class);
+        //startActivity(intent);
+    }
+
+    public void goToOlivierDetails (View view){
+        //Intent intent = new Intent(this, OlivierMealActivity.class);
+        //startActivity(intent);
+    }
+
+    public void goToRUDetails (View view){
+        //Intent intent = new Intent(this, RUMealActivity.class);
+        //startActivity(intent);
     }
 }
