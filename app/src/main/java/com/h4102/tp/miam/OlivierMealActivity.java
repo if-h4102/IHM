25a74dda1d74de935f23ea0a3387b2d9c3795c0d
SebastienDiGiovanni <sebastien.di-giovanni@insa-lyon.fr>
2017-01-12 11:21:03 +0100

    Add all restaurants menu

---
+++ app/src/main/java/com/h4102/tp/miam/OlivierMealActivity.java
@@ -0,0 +1,13 @@
+package com.h4102.tp.miam;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class OlivierMealActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_olivier_meal);
+    }
+}
---
+++ app/src/main/java/com/h4102/tp/miam/RestaurantInsaMealActivity.java
@@ -0,0 +1,13 @@
+package com.h4102.tp.miam;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class RestaurantInsaMealActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_restaurant_insa_meal);
+    }
+}
