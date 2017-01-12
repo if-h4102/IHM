04d15b6a414fccd0503d036cc5d5086d8ce44a5f
Nicolas <nicolas.six@insa-lyon.fr>
2017-01-12 11:36:21 +0100

    A meal can now be organized from every menu

--- app/src/main/java/com/h4102/tp/miam/RuJussieuMealActivity.java
+++ app/src/main/java/com/h4102/tp/miam/RuJussieuMealActivity.java
@@ -1,7 +1,9 @@
 package com.h4102.tp.miam;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 
 public class RuJussieuMealActivity extends AppCompatActivity {
 
@@ -9,5 +11,10 @@ public class RuJussieuMealActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_ru_jussieu_meal);
+    }
+
+    public void organiseMeal(View view){
+        Intent intent = new Intent(this, Organisation.class);
+        startActivity(intent);
     }
 }
